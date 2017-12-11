2c75d5fbdcc4a4b40b53b9591554521225fdfc4f
bigbert836 <brett-barnes@northwestern.edu>
2017-12-11 17:55:57 -0600

    push to friends api

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,7 +8,25 @@ module.exports = function(app) {
 
   
   app.post("/api/friends", function(req, res) {
-   
+  
+  //Fuctions to find total
+  function getSum(total, num) {
+    return total + num;
+  }
+
+  var score = req.body.scores;
+  var match;
+  var matchScore = 0;
+
+  for(var i = 0; i < friends.length; i++){
+    matchScore = friends[i].scores.reduce(getSum);
+    //match = score - matchScore;
+  }
+
+  res.json(matchScore);
+
+   friends.push(req.body);
   });
 
 };
+    
